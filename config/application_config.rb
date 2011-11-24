928c6523a82f25948a041fc59bfce0c848d23c63
Adam Krieg <adammkrieg@gmail.com>
2011-11-24 01:21:07 -0500

    First commit

---
+++ config/application_config.rb
@@ -0,0 +1,2 @@
+configatron.community_name = "JCFC Member Site"
+# See CE's application_config.rb to see all the other configuration options available
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JcfcMembers::Application.config.secret_token = 'feda34688e88421298436b00a9d396c8dbea7891543ed371e1e220ce58db4214d3bd58fb13746c820dd703579dbf36b7c904e5166a41367260c9f14d99ed3f43'
